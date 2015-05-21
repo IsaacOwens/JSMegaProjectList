7100e77bdf12f35b7998ee44a40d3bf5fe8faab8
Isaac Owens <isaacowens@outlook.com>
2015-05-21 16:11:57 -0600

    New functions/projects, plus UI adjustments
    These projects and associated functions were added:
    
    -Display Fibonacci sequence up to the Nth number
    -Find all prime factors in a number
    -Find the next prime number
    
    Submit buttons for first two number projects were removed and now the
    functions are triggered on input.

--- js/jsFunctions.js
+++ js/jsFunctions.js
@@ -8,42 +8,140 @@ function testAlert()
 /***********NUMBERS*************/
 
 /*Find PI to the Nth Digit*/
-var piCountSubmit = document.getElementById("piCountSubmit");
 var piInput = document.getElementById("piInput");
 var piOutput = document.getElementById("piOutput");
 var pi = "1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
 
-piCountSubmit.onclick = function piCount()
+piInput.oninput = function piCount()
 {
 	
 	if (piInput.value > 0 && piInput.value < 101)
-	{
 		piOutput.value = "3." + pi.substring(0,piInput.value);
-	}
 	else
-	{
-		alert("n must be a number between 1 and 100");
-	}
+		piOutput.value = "n must be a number between 1 and 100";
 	
 };
 
 
 /*Find e to the Nth Digit*/
-var eCountSubmit = document.getElementById("eCountSubmit");
 var eInput = document.getElementById("eInput");
 var eOutput = document.getElementById("eOutput");
 var e = "7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274";
 
-eCountSubmit.onclick = function eCount()
+eInput.oninput = function eCount()
 {
 	
 	if (eInput.value > 0 && eInput.value < 101)
-	{
 		eOutput.value = "2." + e.substring(0,eInput.value);
+	else
+		eOutput.value = "n must be a number between 1 and 100";
+	
+};
+
+
+/*Display Fibonacci sequence up to the Nth number*/
+var fibInput = document.getElementById("fibInput");
+var fibOutput = document.getElementById("fibOutput");
+
+fibInput.oninput = function fib()
+{
+	var seq = "";
+	var n = fibInput.value;
+	var s = 0;
+	var t1 = 0;
+	var t2 = 1;
+	
+	if (fibInput.value > 0 && fibInput.value < 10001)
+	{
+		
+		if (n < 2)
+		{
+			seq = "0 1 ";
+		}
+		else
+		{
+			do
+			{
+				seq = seq + s + " ";
+				s = t1 + t2;
+				t1 = t2;
+				t2 = s;
+			} while (s < n);
+		};
+		
+		fibOutput.value = seq;
 	}
 	else
 	{
-		alert("n must be a number between 1 and 100");
-	}
-	
+		fibOutput.value = "n must be a number between 1 and 10,000";
+	};
 };
+
+
+
+/*Find all prime factors in a number*/
+var primeInput = document.getElementById("primeInput");
+var primeOutput = document.getElementById("primeOutput");
+
+primeInput.oninput = function prime()
+{
+	
+	if (primeInput.value > 0 && primeInput.value < 100000001)
+	{
+		primeOutput.value = primeFactorization(primeInput.value);
+	}
+	else
+	{
+		primeOutput.value = "n must be a number between 1 and 100,000,000";
+	};
+};
+
+//primeFactorization function from: http://www.coderenaissance.com/2011/06/finding-prime-factors-in-javascript.html
+function primeFactorization(num){
+  var root = Math.sqrt(num),  
+  result = arguments[1] || [],
+  x = 2; 
+  
+  if(num % x)
+  {
+	x = 3;
+	while((num % x) && ((x = x + 2) < root))
+	{
+	}
+  }
+
+  x = (x <= root) ? x : num;
+  result.push(x);
+
+  return (x === num) ? result : primeFactorization(num/x, result) ;
+}
+
+
+/*Find the next prime number*/
+var nextPrimeButton = document.getElementById("nextPrimeButton");
+var nextPrimeOutput = document.getElementById("nextPrimeOutput");
+
+nextPrimeButton.onclick = function nextPrime()
+{
+	if (nextPrimeOutput.value > 0 && nextPrimeOutput.value < 100000001)
+	{
+		do 
+		{		
+		nextPrimeOutput.value++;
+		} while (isPrime(nextPrimeOutput.value) == false)
+	}
+};
+
+//isPrime function from: http://www.javascripter.net/faq/numberisprime.htm
+function isPrime(n)
+{
+	if (isNaN(n) || !isFinite(n) || n%1 || n<2) return false; 
+	if (n%2==0) return (n==2);
+	if (n%3==0) return (n==3);
+	var m=Math.sqrt(n);
+	for (var i=5;i<=m;i+=6) {
+	if (n%i==0)     return false;
+	if (n%(i+2)==0) return false;
+ }
+ return true;
+}
